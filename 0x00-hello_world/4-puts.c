d171f4c9620e5f8168b0aa430739c9ba7443875d
Sendanz <akpobomehoghenerukevwe@gmail.com>
2023-10-05 19:41:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 /**
  * main - A program that prints a puts function
- *
- * Return: 0 (SUCCESS)
+ * Return: Always 0 (SUCCESS)
  */
 int main(void)
 {
 puts("\" Programming is like building a multilingual puzzle
 ");
-return: (0);
+return (0);
 }
+
