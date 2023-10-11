9e8df189d9102ba5f7e153098ec484647b5b49f6
Sendanz <akpobomehoghenerukevwe@gmail.com>
2023-10-11 12:39:50 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - print the alphabet x10 times
- *
- * Description - This function prints the English alphabet 10 times, with each set of alphabets separated by a newline character
- *
+ * Description - This function prints the English alphabet 10 times, separated by a newline character
  * Return: void
  */
 void print_alphabet_x10(void)
