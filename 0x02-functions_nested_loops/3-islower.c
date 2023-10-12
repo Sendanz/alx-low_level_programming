4a90b27a1be456ccefbb64b51a4e8e54c74de7ea
Sendanz <akpobomehoghenerukevwe@gmail.com>
2023-10-11 17:26:10 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,8 @@
 /**
  * _islower - checks for lower case character
  * @c: The character to be checked 
- * Return: 1 for lower case character or 0 for anything else
+ * Return:1 for lower case character or 0 for anything else
  */
-
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
@@ -14,5 +13,4 @@ return (1);
 }
 return (0);
 }
-
 
